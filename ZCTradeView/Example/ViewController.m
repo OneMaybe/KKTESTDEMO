8035fa4f96514216f6f41bb4400a95ba5b0b83a0
KellenYang <15000642514@163.com>
2015-08-05 18:46:34 +0800

    ADD ZC

---
+++ ZCTradeView/Example/ViewController.m
@@ -0,0 +1,36 @@
+//
+//  ViewController.m
+//  Example
+//
+//  Created by 塔利班 on 15/5/22.
+//  Copyright (c) 2015年 联创智融. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "ZCTradeView.h"
+
+@interface ViewController ()<ZCTradeViewDelegate>
+
+@end
+
+@implementation ViewController
+
+- (IBAction)showTradeView:(id)sender
+{
+//    [[[ZCTradeView alloc] init] show];
+    ZCTradeView *t = [[ZCTradeView alloc] init];
+    t.delegate = self;
+    [t show];
+}
+
+
+-(NSString *)finish:(NSString *)pwd{
+    
+    NSLog(@"%@",pwd);
+    return pwd;
+}
+
+@end
+// 版权属于原作者
+// http://code4app.com (cn) http://code4app.net (en)
+// 发布代码于最专业的源码分享网站: Code4App.com
